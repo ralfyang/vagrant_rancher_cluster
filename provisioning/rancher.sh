e499bb6a3a2db4c73335a3560abecf3b579f5e17
Ralf Yang <goody80762@gmail.com>
2017-03-06 14:54:16 +0900

    update for Docker-CE install

--- provisioning/rancher.sh
+++ provisioning/rancher.sh
@@ -5,9 +5,36 @@
 #vagrant plugin install vagrant-libvirt 
 #vagrant plugin install vagrant-mutate
 
-wget -qO- https://get.docker.com/ | sh
-sudo usermod -aG docker root
-service docker start
+sudo apt-get remove docker docker-engine
+sudo apt-get install \
+    linux-image-extra-$(uname -r) \
+    linux-image-extra-virtual
+
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common -y
+
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo apt-key fingerprint 0EBFCD88
+
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+
+sudo apt-get update
+
+sudo apt-get install docker-ce -y
+
+apt-cache madison docker-ce
+
+
 
 # Docker-compose install
 curl -L https://github.com/docker/compose/releases/download/1.8.0/docker-compose-`uname -s`-`uname -m` > /usr/bin/docker-compose
